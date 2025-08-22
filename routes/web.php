aa4a53f794855bb1015f388b165931a7aad64338
gokulprajapat <gokul.dabang@gmail.com>
2025-08-22 14:32:37 +0530

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome');
+// })->name('home');
+
+// Route::get('dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// require __DIR__.'/settings.php';
+// require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/account-dashboard',[UserController::class,'index'])->name('user.index');
+});
+
+Route::middleware(['auth',AuthAdmin::class])->group(function(){
+    Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
+});
