4268fd0ca1a9990505a9f927f4f83435f67191aa
gokulprajapat <gokul.dabang@gmail.com>
2025-09-03 08:57:40 +0530

    brand edit view file added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -42,12 +42,17 @@ class AdminController extends Controller
         return redirect()->route('admin.brands')->with("status","Brand has been added successfully!");
     }
 
+    public function brand_edit($id){
+        $brand=Brand::find($id);
+        return  view("admin.brand-edit",compact('brand'));
+    }
+
     public function GenerateBrandThumbnailImage($image,$imagename){
         $destinationPath=public_path("uploads/brands");
         $img=Image::read($image->path());
         $img->cover(124,124,'top');
         $img->resize(124,124,function($constraint){
-            $constraint->aspectRation();
+            $constraint->aspectRatio();
         })->save($destinationPath.'/'.$imagename);
     }
 }
