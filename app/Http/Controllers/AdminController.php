01d81436fb77b88a6ec14cda0adcefedb2c9fd90
gokulprajapat <gokul.dabang@gmail.com>
2025-09-11 08:08:06 +0530

    Category added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\File;
@@ -88,5 +89,10 @@ class AdminController extends Controller
         }
         $brand->delete();
         return redirect()->route('admin.brands')->with("status","Brand has been deleted successfully");
+    }
+
+    public function categories(){
+        $categories=Category::orderBy('id','desc')->paginate(10);
+        return view('admin.categories',compact('categories'));
     }
 }
