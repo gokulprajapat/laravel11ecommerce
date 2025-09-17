2e6655c7663c503c57d484a8dd93a74b20a2bd26
gokulprajapat <gokul.dabang@gmail.com>
2025-09-17 08:37:21 +0530

    product add

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -178,4 +178,22 @@ class AdminController extends Controller
         return view('admin.product-add',compact('categories'),compact('brands'));
     }
 
+    public function product_store(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'slug'=>'required|unique:products,slug',
+            'short_description'=>'required',
+            'description'=>'required',
+            'regular_price'=>'required',
+            'sale_price'=>'required',
+            'SKU'=>'required',
+            'stock_status'=>'required',
+            'featured'=>'required',
+            'quantity'=>'required',
+            'image'=>'required',
+            'images'=>'required',
+            'category_id'=>'required',
+            'brand_id'=>'required'
+        ]);
+    }
 }
